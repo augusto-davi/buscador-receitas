a9b4b19338d8769420590ff1448bcaaa0fcba9b5
Davi <obrigatoriopcional@gmail.com>
2025-07-15 16:46:10 -0300

    add input form

--- src/app/receita-busca/receita-busca.ts
+++ src/app/receita-busca/receita-busca.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-receita-busca',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './receita-busca.html',
   styleUrl: './receita-busca.scss'
 })
 export class ReceitaBusca {
+  ingrediente = '';
 
+  buscarReceitas(){
+    console.log('Buscando receitas com:', this.ingrediente);
+  }
 }
